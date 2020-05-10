8a2e062bb2dc59bf4e1ba83a9805ff1fbb8af5d4
澤村裕幸 <sawamura-hi@lakeel.com>
2020-05-11 01:32:14 +0900

    generateから不要ページ削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -166,18 +166,18 @@ const config: Configuration = {
   generate: {
     fallback: true,
     routes() {
-      const locales = ['ja', 'en', 'zh-cn', 'zh-tw', 'ko', 'ja-basic']
+      const locales = ['ja', 'ja-basic']
       const pages = [
-        '/cards/details-of-confirmed-cases',
-        '/cards/details-of-tested-cases',
+        // '/cards/details-of-confirmed-cases',
+        // '/cards/details-of-tested-cases',
         '/cards/number-of-confirmed-cases',
         '/cards/number-of-confirmed-cases-by-municipalities',
         '/cards/attributes-of-confirmed-cases',
-        '/cards/number-of-tested',
-        '/cards/number-of-inspection-persons',
-        '/cards/number-of-reports-to-covid19-telephone-advisory-center',
+        // '/cards/number-of-tested',
+        // '/cards/number-of-inspection-persons',
+        // '/cards/number-of-reports-to-covid19-telephone-advisory-center',
         '/cards/number-of-reports-to-covid19-telephone-advisory-center-custom',
-        '/cards/number-of-reports-to-covid19-consultation-desk',
+        // '/cards/number-of-reports-to-covid19-consultation-desk',
         '/cards/predicted-number-of-toei-subway-passengers',
         '/cards/agency'
       ]
